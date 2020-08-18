e145ca9a4c5d385540d42d34ce797aa4b81d55ee
Alexis Montes <montes.alexis25@gmail.com>
2020-08-17 23:05:04 -0400

    started initial footer design

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+function Footer(){
+
+    return(
+        <footer id="footer" className="page-footer">
+            <div>
+
+            </div>
+            <div className="footer-copyright text-center py-3">
+                <div>
+                    <p>
+                        Coded with love and Coffee
+                    </p>
+                </div>
+            </div>
+
+        </footer>
+
+    );
+}
+
+export default Footer;
